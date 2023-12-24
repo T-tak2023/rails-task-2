906812c7743ad2856bb3a1bf7ec7be3590a8ce8a
USER <user@USERnoMacBook-Pro.local>
2023-12-24 15:02:49 +0900

    home画面を作成し、rootメソッドの遷移先として設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions:      'users/sessions'
   }
   resources :users, only: [:show]
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
