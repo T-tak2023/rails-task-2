78728fff8bbfc860764a47245b2d51d4ef9ad61b
USER <user@USERnoMacBook-Pro.local>
2023-12-24 13:12:47 +0900

    userのshowビューを作成、サインアップ後のリダイレクト先としてルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
     registrations: 'users/registrations',
     sessions:      'users/sessions'
   }
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
