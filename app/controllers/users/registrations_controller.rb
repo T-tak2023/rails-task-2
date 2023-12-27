eca11c638478c3b965955e079b8810ff065ab018
USER <user@USERnoMacBook-Pro.local>
2023-12-27 19:56:36 +0900

    アカウント編集後のリダイレクト先をアカウントページに設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,7 +56,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   def after_update_path_for(resource)
-    user_path(resource.id)
+    users_account_path
   end
 
   # The path used after sign up.
