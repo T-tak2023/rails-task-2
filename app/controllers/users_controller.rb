dd8cd4f0481bcb1ca79359dffafce1bd4e963676
USER <user@USERnoMacBook-Pro.local>
2023-12-27 12:32:12 +0900

    他のユーザーのアカウントページにアクセスできないようshowアクションに定義を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def show
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+    redirect_to user_path(current_user)
+    end
     @user = User.find(params[:id])
   end
 end
