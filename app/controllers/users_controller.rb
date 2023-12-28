21f3832870a9bb9ad46f90c17a80ea995f051182
USER <user@USERnoMacBook-Pro.local>
2023-12-28 13:18:21 +0900

    profile_edit, profile_updateアクションを追加し、プロフィール編集画面を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:account, :profile]
+  before_action :authenticate_user!, only: [:account, :profile,:profile_edit, :profile_update]
   def account
-    @user = User.find_by(id: current_user.id)
+    @user = current_user
   end
 
   def profile
-    @user = User.find_by(id: current_user.id)
+    @user = current_user
+  end
+
+  def profile_edit
+    @user = current_user
+  end
+
+  def profile_update
+    @user = current_user
+    if @user.update(profile_params)
+      redirect_to users_profile_path
+    else
+      render "profile_edit"
+    end 
+  end
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:profile_update, keys: [:name, :introduction])
+  end
+    
+  def profile_params
+    params.require(:user).permit(:name, :introduction)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,10 @@ Rails.application.routes.draw do
   }
   get 'home/index'
   root to: 'home#index'
-  resources :users , only: [:account, :profile]
+  resources :users , only: [:account, :profile, :profile_edit, :profile_update]
   get 'users/account' => 'users#account'
   get 'users/profile' => 'users#profile'
+  get 'users/profile/edit' => 'users#profile_edit'
+  patch 'users/profile/edit' => 'users#profile_update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
