90b741053e2b59d5480ec5ced4fc50f7a239ea52
USER <user@USERnoMacBook-Pro.local>
2024-01-03 20:52:13 +0900

    施設一覧の施設名を施設詳細へのリンクに設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index, :new, :create]
   def index
     @rooms = Room.where(user_id: current_user.id)
   end
@@ -16,6 +16,11 @@ class RoomsController < ApplicationController
       render "new"
     end
   end
+
+  def show
+    @room = Room.find(params[:id])
+  end
+
   private
     def room_params
       params.require(:room).permit(:room_name, :room_introduction, :price_per_day, :room_address, :room_image, :user_id)
