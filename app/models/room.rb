a945c0bbccc5d6a8067a3ebd2218db00d7e00f01
USER <user@USERnoMacBook-Pro.local>
2024-01-03 15:26:13 +0900

    施設登録時のバリデーションを設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ApplicationRecord
   mount_uploader :room_image, RoomImageUploader
   belongs_to :user
+  validates :room_name, presence: true
+  validates :room_introduction, presence: true
+  validates :price_per_day, presence: true, numericality: {only_integer:true, greater_than_or_equal_to: 1}
+  validates :room_address, presence: true
 end
