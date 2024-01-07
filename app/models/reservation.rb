715c2b2307145ef2bc2665afbde2b4b769e9d9d4
USER <user@USERnoMacBook-Pro.local>
2024-01-07 14:55:39 +0900

    予約人数が1名以上になるようバリデーションを追加、number_fieldの入力を1..10に制限

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,5 +2,5 @@ class Reservation < ApplicationRecord
   belongs_to :user
   validates :check_in_date, presence: true
   validates :check_out_date, presence: true
-  validates :person, presence: true
+  validates :person, presence: true, numericality: {greater_than_or_equal_to: 1}
 end
